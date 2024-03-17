49acc42d74739047373d71d3ec2f7d349299d27f
Lukas Hilfiker <lukas@hilfiker.dev>
2024-03-17 20:25:40 +0100

    ignore permission errors

--- filesystem-searcher-poc/main.cpp
+++ filesystem-searcher-poc/main.cpp
@@ -8,7 +8,7 @@ namespace fs = std::filesystem;
 
 std::vector<fs::path> get_paths(fs::path path, fs::file_time_type datetime) {
 	std::vector<fs::path> paths;
-	for (const auto& dir_entry : fs::recursive_directory_iterator(path)) {
+	for (const auto& dir_entry : fs::recursive_directory_iterator(path, fs::directory_options::skip_permission_denied)) {
 			std::error_code ec;
 			if (!fs::is_directory(dir_entry, ec) && fs::last_write_time(dir_entry, ec) > datetime) { // No read, is text file checks because that will the indexer do.
 				paths.push_back(dir_entry.path());
