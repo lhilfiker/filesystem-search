a4881cbc9745d569f61941330bda3dee698c4cf0
Lukas Hilfiker <lukas@hilfiker.dev>
2024-03-17 14:00:19 +0100

    check if entered path is valid

--- filesystem-searcher-poc/main.cpp
+++ filesystem-searcher-poc/main.cpp
@@ -14,7 +14,7 @@ std::vector<fs::path> get_paths(fs::path path, fs::file_time_type datetime) {
 	for (const auto& dir_entry : fs::recursive_directory_iterator(path)) {
 			std::error_code ec;
 			const auto ftime = fs::last_write_time(dir_entry, ec);
-			if (!fs::is_directory(dir_entry, ec) && ftime > datetime) {
+			if (!fs::is_directory(dir_entry, ec) && ftime > datetime) { // No read, is text file checks because that will the indexer do.
 				paths.push_back(dir_entry.path());
 			}
 			if (ec) {
@@ -26,16 +26,21 @@ std::vector<fs::path> get_paths(fs::path path, fs::file_time_type datetime) {
 
 int main() 
 {
-	fs::path path = "/";
-	std::cin >> path;
+	fs::path path = "/home/user/git/filesystem-search/";
 	// This is just to get a file_time_type for the PoC. In the final implementation a file_time_type will be provided here.	
-	fs::file_time_type datetime = fs::last_write_time("/");
+	fs::file_time_type datetime = fs::last_write_time("/home/user/git/filesystem-search/readme.md");
+	
+	std::error_code ec;
+	if(!fs::is_directory(path,ec)) return 4;
+	if(ec) return 1;
 
 	std::vector<fs::path> paths = get_paths(path, datetime);
 
 	for (fs::path path : paths) {
 		std::cout << path << "\n";
 	}	
+
+	return 0;
 
 
 }	
