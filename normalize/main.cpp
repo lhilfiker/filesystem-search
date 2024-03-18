9c44b1d923666aab370f6ddf2bbff3f444a505d3
Lukas Hilfiker <lukas@hilfiker.dev>
2024-03-18 09:53:40 +0100

    improve not allowed char dedection. only a-z 0-9 is allowed. A-Z will be lowercased.

--- normalize/main.cpp
+++ normalize/main.cpp
@@ -4,13 +4,13 @@
 #include <algorithm>
 #include <cctype>
 std::vector<std::string> normalize(std::vector<std::string> input) {
-	static const char invalid[] = {'.', ',', '!', '?'};
+	static const char allowed[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	static std::string stopwords[] = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each", "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him", "himself", "his", "how", "hows", "i", "id", "ill", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shant", "she", "shed", "shell", "shes", "should", "shouldnt", "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "wed", "well", "were", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres", "which", "while", "who", "whos", "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves"};
 	int i = 0;
 	for (std::string word : input) {
 		std::string updated = "";
 		for (const char c : word) { // remove invalid 
-			if (!std::count(std::begin(invalid), std::end(invalid), c)) {
+			if (std::count(std::begin(allowed), std::end(allowed), c)) {
 				updated += std::tolower(c);
 			}
 		}
