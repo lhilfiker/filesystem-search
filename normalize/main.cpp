253820507a9d22a71a8ffdb42addb6238066884f
Lukas Hilfiker <lukas@hilfiker.dev>
2024-03-18 09:21:05 +0100

    normalize: remove symbols and empty words

--- normalize/main.cpp
+++ normalize/main.cpp
@@ -2,8 +2,26 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
-
+#include <cctype>
 std::vector<std::string> normalize(std::vector<std::string> input) {
+	const char invalid[] = {'.', ',', '!', '?'};
+	int i = 0;
+	for (std::string word : input) {
+		std::string updated = "";
+		for (const char c : word) { // remove invalid 
+			if (!std::count(std::begin(invalid), std::end(invalid), c)) {
+				updated += std::tolower(c);
+			}
+		}
+		if (updated != "") {
+			input[i] = updated;
+			++i;
+		}
+		else {
+			input.erase(input.begin() + i); // if updated string is empty, delete it.
+		}
+	}
+	// remove duplicates
 	std::sort(input.begin(), input.end());
 	auto it = std::unique(input.begin(), input.end());
 	input.erase(it, input.end());
