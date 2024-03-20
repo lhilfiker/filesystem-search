eb1ba167c7d75d328e28dc25345e22b116d15a0a
Lukas Hilfiker <lukas@hilfiker.dev>
2024-03-20 10:54:40 +0100

    option to not index files/paths starting with a dot #10

--- background-app/forward_index_from_file_paths/forward_index_from_file_paths.cpp
+++ background-app/forward_index_from_file_paths/forward_index_from_file_paths.cpp
@@ -20,12 +20,12 @@ std::vector<std::string> string_to_vector(std::string input) {
 	return vstrings;
 }
 
-std::vector<DocumentIndex> forward_index_from_paths(std::vector<std::filesystem::path> paths) {
+std::vector<DocumentIndex> forward_index_from_paths(std::vector<std::filesystem::path> paths, bool ignore_files_with_dot) {
 	static std::unordered_set<std::string> notallowed{".png", ".jpeg", ".mp4", ".mp3", ".wav", ".avi", ".mov", ".mkv", ".flv", ".wmv", ".m4a", ".flac", ".ogg", ".gif", ".bmp", ".tif", ".tiff", ".psd", ".eps", ".raw", ".cr2", ".nef", ".orf", ".sr2", ".webp", ".3gp", ".3g2", ".m4v", ".mpg", ".mpeg", ".m2v", ".svi", ".3gp", ".3g2", ".mxf", ".roq", ".nsv", ".flac", ".mid", ".midi", ".wma", ".aac", ".wav", ".ogg", ".oga", ".mka", ".ts", ".jpg", ".heic", ".arw", ".dng", ".dwg", ".dxf", ".ico", ".svg", ".webm", ".lrv", ".m2ts", ".mts", ".divx", ".dat", ".bin", ".iso", ".vob", ".mod", ".m2p", ".m2v", ".m4p", ".m4v", ".mp2", ".mpe", ".mpeg", ".mpg", ".mpv2", ".mts", ".nsv", ".ogm", ".ogv", ".qt", ".ram", ".rm", ".rmvb", ".tod", ".ts", ".vob", ".wm", ".yuv", ".doc", ".docx", ".pdf", ".xls", ".xlsx", ".ppt", ".pptx", ".odb", ".odg", ".odp", ".ods", ".odt", ".pages", ".key", ".numbers", ".ai", ".indd", ".pub", ".xps", ".3dm", ".max", ".obj", ".blend", ".c4d", ".ma", ".mb", ".dae", ".3ds", ".fbx", ".stl", ".skp", ".vrml", ".x3d", ".sldprt", ".sldasm", ".ipt", ".iam", ".dwf", ".dwt", ".idw", ".stl", ".ply", ".pct", ".pcx", ".pic", ".mrw", ".j2k", ".jpf", ".jpx", ".jpm", ".mj2", ".tga", ".dds", ".jxr", ".hdp", ".wdp", ".cur", ".ani"};
 	std::vector<DocumentIndex> all;
 	for (std::filesystem::path current_path : paths) {
 		std::error_code ec;
-		if (!std::filesystem::exists(current_path) || notallowed.find(current_path.extension()) != notallowed.end()) continue; // files with specific extension, do not even try to read it.
+		if (!std::filesystem::exists(current_path) || notallowed.find(current_path.extension()) != notallowed.end() || current_path.string().find("/.") == true) continue; // files with specific extension, do not even try to read it.
 		auto filesize = std::filesystem::file_size(current_path);
 		if (filesize > 20971520) continue; // skip if file is too large, in the future this can be customized.
 		std::string content;
@@ -48,7 +48,7 @@ int main() {
 		paths.push_back(d);
 	}
 
-	std::vector<DocumentIndex> content = forward_index_from_paths(paths);
+	std::vector<DocumentIndex> content = forward_index_from_paths(paths, true);
 	
 	for (DocumentIndex index : content) {
 		std::cout << "\nDocument: " << index.document_path << ".\n";
